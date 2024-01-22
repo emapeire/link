6fb603717a0ddcdb4b8a65d098a747aa5c1c1507
Emanuel Peire <emapeire@outlook.com>
2024-01-22 18:09:06 -0300

    Update ESLint and remove Prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["standard-with-typescript", "plugin:react/recommended"],
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react"],
-  rules: {},
-};
+  plugins: ['react'],
+  rules: {}
+}

3930572b72962bf5c441131c7ba7da13ca4688f4
Emanuel Peire <emapeire@outlook.com>
2024-01-22 18:55:37 -0300

    Add @vercel/analytics package to package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type React from 'react'
 import { HeadIcons } from 'src/components/head-icons'
-import { metadata } from 'src/components/metadata'
+import { type Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 // import { Analytics } from '@vercel/analytics/react'
+
+const metadata: Metadata = {
+  title: 'Links | Emanuel Peire',
+  description: 'Links to my social media'
+}
 
 const inter = Inter({
   subsets: ['latin'],

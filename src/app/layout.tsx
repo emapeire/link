59dc3a220a6ad9ddd4a89432e0b2ad66deb88687
Emanuel Peire <emapeire@outlook.com>
2024-01-25 11:48:16 -0300

    Add new dependencies and update styles with shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className={`${inter.className}`}>
+    <html lang='en' className={`${inter.className} dark`}>
       <Head metadata={metadata} />
       <body>
         {children}

a244a6b98abbbd32e5ce9b3a5e94811f3c3cf217
Emanuel Peire <emapeire@outlook.com>
2024-02-01 23:27:42 -0300

    Refactor head component metadata tags

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -3,18 +3,22 @@ import { type Metadata } from 'next'
 export function Head({ metadata }: { metadata: Metadata }) {
   return (
     <head>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
       <title>{metadata.title?.toString()}</title>
       <meta name='description' content={metadata.description?.toString()} />
+
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name='author' content='Emanuel Peire' />
       <meta name='robots' content='index, follow' />
-      <meta name='application-name' content='ep-app' />
-      <meta name='apple-mobile-web-app-title' content='ep-app' />
-      <meta name='theme-color' content='#000000' />
+
       <meta name='msapplication-TileColor' content='#000000' />
-      <meta name='msapplication-TileImage' content='/mstile-144x144.png' />
-      <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      <meta name='theme-color' content='#000000'></meta>
+
+      <link
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/apple-touch-icon.png'
+      />
       <link
         rel='icon'
         type='image/png'
@@ -24,28 +28,10 @@ export function Head({ metadata }: { metadata: Metadata }) {
       <link
         rel='icon'
         type='image/png'
-        sizes='194x194'
-        href='/favicon-194x194.png'
-      />
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='192x192'
-        href='/android-chrome-192x192.png'
-      />
-      <link
-        rel='icon'
-        type='image/png'
         sizes='16x16'
         href='/favicon-16x16.png'
       />
-      <link
-        rel='apple-touch-icon'
-        sizes='180x180'
-        href='/apple-touch-icon.png'
-      />
       <link rel='manifest' href='/site.webmanifest' />
-      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#000000' />
     </head>
   )
 }

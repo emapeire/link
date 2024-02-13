1e5037c540509ddcc30146b42e90a83ba15975d3
Emanuel Peire <emapeire@outlook.com>
2024-02-13 15:46:58 -0300

    Add new dependencies and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type React from 'react'
 import { type Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { GeistSans } from 'geist/font/sans'
 import { Head } from '@/components/head'
 import { Footer } from '@/components/footer'
-// import { Analytics } from '@vercel/analytics/react'
+import { ThemeProvider } from '@/components/theme-provider'
 import '@/styles/globals.css'
 
 const metadata: Metadata = {
@@ -11,22 +11,23 @@ const metadata: Metadata = {
   description: 'Links to my social media'
 }
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap'
-})
-
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className={`${inter.className} dark`}>
+    <html lang='en' className={`${GeistSans.className} dark`}>
       <Head metadata={metadata} />
       <body>
-        {children}
-        {/* <Analytics /> */}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
         <Footer />
       </body>
     </html>

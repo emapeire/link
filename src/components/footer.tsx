ef9a7c33d32b551a2afdd2e60eef273f22cee5b2
Emanuel Peire <emapeire@outlook.com>
2024-02-15 23:03:01 -0300

    Update footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className='flex flex-col items-center justify-center py-4 mt-12 mb-16 mx-auto w-full max-w-[14rem] font-semibold text-gray-300 text-center text-sm border border-white/5 bg-white/10 shadow-xl shadow-white/5 rounded-xl'>
+    <footer className='flex flex-col items-center justify-center py-3.5 mt-12 mb-16 mx-auto w-full max-w-[14rem] font-semibold text-gray-300 text-center text-sm border border-white/5 bg-white/10 shadow-xl shadow-white/5 rounded-xl'>
       <div className='flex items-center justify-center space-x-6'>
         <span>{`${year}`}</span>
         <span>|</span>

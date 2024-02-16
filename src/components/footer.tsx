b7b864e0d6db8b69c16f3e32a3d98b7f27d77a57
Emanuel Peire <emapeire@outlook.com>
2024-02-15 22:46:27 -0300

    Refactor footer component layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,18 +4,20 @@ export function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className='flex items-center py-4 mt-12 mb-16 w-full max-w-xl font-semibold justify-evenly text-gray-300 text-center text-sm border border-white/5 bg-white/10 shadow-xl shadow-white/5 rounded-xl'>
-      {`${year}`}
-      <span>|</span>
-      <span className='hover:underline hover:text-blue-400'>
-        <a
-          href={data.socials[1]?.url}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          @emapeire
-        </a>
-      </span>
+    <footer className='flex flex-col items-center justify-center py-4 mt-12 mb-16 w-auto font-semibold text-gray-300 text-center text-sm border border-white/5 bg-white/10 shadow-xl shadow-white/5 rounded-xl'>
+      <div className='flex items-center justify-center space-x-4'>
+        <span>{`${year}`}</span>
+        <span>|</span>
+        <span className='hover:underline hover:text-blue-400'>
+          <a
+            href={data.socials[1]?.url}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            @emapeire
+          </a>
+        </span>
+      </div>
     </footer>
   )
 }

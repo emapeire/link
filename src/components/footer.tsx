088e5322ef50c35815b7360b7dc741f93130c5dc
Emanuel Peire <emapeire@outlook.com>
2024-02-16 20:26:04 -0300

    Update styles in contact-card and footer components

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,22 +1,23 @@
-import { data } from '@/constants'
-
 export function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className='flex flex-col items-center justify-center py-3.5 mt-28 mb-16 mx-auto w-full max-w-[14rem] font-semibold text-gray-300 text-center text-sm border border-white/5 bg-white/10 shadow-xl shadow-white/5 rounded-xl'>
+    <footer className='flex flex-col items-center justify-center py-3.5 mt-28 mb-16 mx-auto w-full max-w-[16rem] font-semibold text-gray-300 text-center text-sm border border-white/5 bg-white/10 shadow-xl shadow-white/5 rounded-xl'>
       <div className='flex items-center justify-center space-x-6'>
         <span>{`${year}`}</span>
         <span>|</span>
-        <span className='hover:underline hover:text-blue-500'>
-          <a
-            href={data.socials[1]?.url}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            @emapeire
-          </a>
-        </span>
+        <section>
+          <span>By</span>{' '}
+          <span className='hover:underline hover:text-blue-500'>
+            <a
+              href='https://x.com/emapeire'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              @emapeire
+            </a>
+          </span>
+        </section>
       </div>
     </footer>
   )
